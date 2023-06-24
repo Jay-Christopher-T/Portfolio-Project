13bc2efad9aab897ed8c3e46579f32988a771eca
jimbo <jamestsakopoulos@gmail.com>
2023-06-23 20:24:46 -0500

    "added sizing and animations to JS about me"

--- interactions.js
+++ interactions.js
@@ -1,14 +1,22 @@
 let viewMeButton = document.getElementById("clickToViewButton");
 let aboutMe = document.getElementById("blurb");
+let aboutMeBox = document.getElementById("aboutMeBox");
+aboutMeBox.style.height="5rem";
+aboutMeBox.style.marginBottom="5rem";
+aboutMeBox.style.width="15rem";
 document.getElementById("clickToHideButton").style.visibility="hidden";
 aboutMe.style.visibility="hidden";
 function unHide () {
     aboutMe.style.visibility="visible";
     viewMeButton.style.visibility="hidden";
+    aboutMeBox.style.height="";
+    aboutMeBox.style.width="";
     document.getElementById("clickToHideButton").style.visibility="visible";
 }
 function hide () {
     aboutMe.style.visibility="hidden";
+    aboutMeBox.style.height="5rem";
+    aboutMeBox.style.width="15rem";
     viewMeButton.style.visibility="visible";
     document.getElementById("clickToHideButton").style.visibility = "hidden";
 }
